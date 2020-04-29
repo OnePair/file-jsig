a9c3c9c37a1f97fc775fa4cd46c9e99f81afcd8d
boumba100 <boumba100@gmail.com>
2020-04-28 21:27:30 -0400

    Build

--- dist/src/main/exceptions/verification-exception.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class VerificationException extends Error {
-    constructor(message: any);
-}
--- dist/src/main/model/jsig-jwts.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class JSigJWTs {
-    private signatures;
-    constructor(signatures?: Map<number, string>);
-    getSignatures(): Map<number, string>;
-    getLastSigHash(): string;
-    addSignature(signature: string): void;
-    toJson(): string;
-    static fromJson(jsonStr: string): JSigJWTs;
-}
